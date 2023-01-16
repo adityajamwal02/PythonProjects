2aaa35978b949295f64ba0a506e3e5fb0eb6ef10
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-01-17 00:44:49 +0530

    RockPaperScissors

---
+++ RockPaperScissors.py
@@ -0,0 +1,44 @@
+import random
+
+user_wins=0
+computer_wins=0
+
+options=["rock", "paper", "scissors"]
+
+while True:
+    user_input=input("Choose Rock/Paper/Scissors or Q to Quit: ").lower()
+    if user_input=="q":
+        break
+    
+    if user_input not in options:
+        continue
+    
+    random_number=random.randint(0,2)
+    # rock->0, paper->1, scissor->2
+    
+    computer_pick=options[random_number]
+    print("Computer picked", computer_pick + ".")
+    
+    if user_input=="rock" and computer_pick=="scissors":
+        print("You Win!")
+        user_wins+=1
+            
+    elif user_input=="scissors" and computer_pick=="paper":
+        print("You Win!")
+        user_wins+=1
+    
+    elif user_input=="paper" and computer_pick=="rock":
+        print("You Win!")
+        user_wins+=1
+    
+    elif user_input==computer_pick:
+        print("Draw!")
+        
+    else:
+        print("You Lost!")
+        computer_wins+=1
+
+
+print("You won", user_wins, "times.")
+print("Computer won", computer_wins, "times.")
+print("Goodbye!")
